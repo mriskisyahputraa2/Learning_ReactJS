dd4131f82b98b7f3a599dfc6f6419baebf227fbb
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-04-17 09:52:59 +0700

    add clock

--- learn-doc-react/src/components/Clock.jsx
+++ learn-doc-react/src/components/Clock.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 
 function Clock() {
+  // membuat state dengan waktu saat ini
   const [currentTime, setCurrentTime] = useState(new Date());
 
+  // proses membuat detik
   useEffect(() => {
+    // menjalankan fungsi detik dengan setInterval yang disimpan dalam v-interValid
     const interValId = setInterval(() => {
+      // data setInterval sudah jalan selama satu detik, trs di ubah menjadi waktu sekarang
       setCurrentTime(new Date());
-    }, 1000);
+    }, 1000); // 1 detik
 
+    // dan bersihkan(unmount) detiknya dengan clearInterval
     return () => clearInterval(interValId);
   }, []);
 
+  // mengubah objek wakt menjadi format jam yang sesuai
   function formatTime(date) {
     const hours = date.getHours().toString().padStart(2, 0);
     const minutes = date.getMinutes().toString().padStart(2, 0);
@@ -30,3 +36,9 @@ function Clock() {
   );
 }
 export default Clock;
+
+// setInterval : adalah sebuah function yang menjalankan sebuah fungsi secara berulang-ulang setiap waktu yang di tentukan dalam milidetik.
+
+// ClearInterval: adalah sebuah function yang digunakan untuk menghentikan setInterval setiap detik yang di setInterval
+
+// setCurrentTime: digunakan untuk mengubah nilai dari sebuah state contoh diatas adalah state currentTime, mengubahnya menjadi waktu sekarang
