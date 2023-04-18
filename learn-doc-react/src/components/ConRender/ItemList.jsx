e3c03fcc04895ab070591f931013ca3c9f97b4b8
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-04-18 22:57:50 +0700

    conditional rendering

--- learn-doc-react/src/components/ConRender/ItemList.jsx
+++ learn-doc-react/src/components/ConRender/ItemList.jsx
@@ -1,20 +1,29 @@
 function ItemList({ name, status }) {
+    let itemContent = name;
+
+    if (status) {
+        itemContent = name + ' (true)';
+    }
+
     return <>
         {/* Use ternary operator */}
-        <div className="text-lg font-semibold">
+        <div className="text-lg font-semibold my-3">
             {status ? (
                 <del className="text-green-600">
-                    Use Ternary: {name} '(true)'
+                    Use Ternary: {name} (true)
                 </del>
             ) : (
                 <p className="text-red-600">Use Ternary {name} (false)</p>
             )}
             <hr />
-        </div>
 
-        {/* Use Operator && */}
-        <div className="text-fuchsia-600 font-thin">
-            Use &&: {name} {status && 'true'}
+            {/* Use Operator && */}
+            <div className="text-fuchsia-600 font-thin">
+                Use Operator &&: {name} {status && 'true'}
+            </div>
+
+            {/* Use Conditional Assigning (penugasan) to a variabel  */}
+            <b className="font-light">Use Assigning(penugasan): {itemContent}</b>
         </div>
     </>
 }
